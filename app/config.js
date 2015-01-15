708a2b0b07a8cb707555b1b145626cc1aa938141
Hack Reactor Students <accounts@hackreactor.com>
2015-01-14 21:10:21 -0800

    Refactoring mongo

--- app/config.js
+++ app/config.js
@@ -1,46 +1,57 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
+// var Bookshelf = require('bookshelf');
+// var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+mongoose.connect('mongodb://localhost/shortlyDb');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: process.env.host,
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    // port: '27017',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
-});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(callback){
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
+
+// ----------------URL--------------------------------
+  var urlSchema = mongoose.Schema({
+    title: String,
+    url: String,
+    base_url: String,
+    code: String,
+    visits: Number
+  });
+
+  var Url = mongoose.model('Url', urlSchema);
+
+  urlSchema.method.createCode = function() {
+    var shasum = crypto.createHash('sha1');
+    this.url = shasum;
+    this.code = shasum.digest('hex').slice(0, 5);
+  };
+
+
+// ------------USERS------------------------------------
+  var usersSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    salt: String
+  });
+
+  var User = mongoose.model('User', usersSchema);
+
+  usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+      callback(isMatch);
     });
-  }
-});
+  },
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
+  usersSchema.methods.hashPassword = function(){
+    bcrypt.genSalt(10, function(error, result) {
+      this.salt = result;
+      bcrypt.hash(this.password, this.salt, null, function(error, res) {
+        this.password = res;
+      });
     });
-  }
+  };
+
 });
 
 module.exports = db;
